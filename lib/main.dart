bbc3b8b7221a7507cc36d6804221f030928b5728
Dagon <leedagon624@gmail.com>
2023-06-09 01:20:53 +0900

    voice added

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,19 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
-import 'dart:convert' as convert;
 import 'package:camera/camera.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 import 'package:http/http.dart' as http;
 import 'package:http_parser/http_parser.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 Future<void> main() async{
   // Ensure that plugin services are initialized so that `availableCameras()`
   // can be called before `runApp()`
   WidgetsFlutterBinding.ensureInitialized();
-
   // Obtain a list of the available cameras on the device.
   final cameras = await availableCameras();
-
+  
   // Get a specific camera from the list of available cameras.
   final firstCamera = cameras.first;
   runApp(MaterialApp(
@@ -26,11 +26,6 @@ Future<void> main() async{
     ));
 }
 
-class MyAppState extends ChangeNotifier {
-  void getNext() {
-    notifyListeners();
-  }
-}
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title, required this.camera,});
@@ -39,8 +34,133 @@ class MyHomePage extends StatefulWidget {
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
-
+enum TtsState { playing, stopped, paused, continued }
 class _MyHomePageState extends State<MyHomePage> {
+  late FlutterTts flutterTts;
+  String language = "ko";
+  String? engine;
+  double volume = 0.5;
+  double pitch = 1.0;
+  double rate = 0.5;
+  bool isCurrentLanguageInstalled = false;
+
+  TtsState ttsState = TtsState.stopped;
+  get isPlaying => ttsState == TtsState.playing;
+  get isStopped => ttsState == TtsState.stopped;
+  get isPaused => ttsState == TtsState.paused;
+  get isContinued => ttsState == TtsState.continued;
+
+  bool get isIOS => !kIsWeb && Platform.isIOS;
+  bool get isAndroid => !kIsWeb && Platform.isAndroid;
+  bool get isWindows => !kIsWeb && Platform.isWindows;
+  bool get isWeb => kIsWeb;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    initTts();
+    _speak("안녕하세요 My Memory 입니다 환영합니다.");
+  }
+  initTts() {
+    flutterTts = FlutterTts();
+
+    _setAwaitOptions();
+
+    if (isAndroid) {
+      _getDefaultEngine();
+      _getDefaultVoice();
+    }
+
+    flutterTts.setStartHandler(() {
+      setState(() {
+        print("Playing");
+        ttsState = TtsState.playing;
+      });
+    });
+
+    if (isAndroid) {
+      flutterTts.setInitHandler(() {
+        setState(() {
+          print("TTS Initialized");
+        });
+      });
+    }
+
+    flutterTts.setCompletionHandler(() {
+      setState(() {
+        print("Complete");
+        ttsState = TtsState.stopped;
+      });
+    });
+
+    flutterTts.setCancelHandler(() {
+      setState(() {
+        print("Cancel");
+        ttsState = TtsState.stopped;
+      });
+    });
+
+    flutterTts.setPauseHandler(() {
+      setState(() {
+        print("Paused");
+        ttsState = TtsState.paused;
+      });
+    });
+
+    flutterTts.setContinueHandler(() {
+      setState(() {
+        print("Continued");
+        ttsState = TtsState.continued;
+      });
+    });
+
+    flutterTts.setErrorHandler((msg) {
+      setState(() {
+        print("error: $msg");
+        ttsState = TtsState.stopped;
+      });
+    });
+  }
+
+  Future<dynamic> _getLanguages() async => await flutterTts.getLanguages;
+
+  Future<dynamic> _getEngines() async => await flutterTts.getEngines;
+
+  Future _getDefaultEngine() async {
+    var engine = await flutterTts.getDefaultEngine;
+    if (engine != null) {
+      print(engine);
+    }
+  }
+
+  Future _getDefaultVoice() async {
+    var voice = await flutterTts.getDefaultVoice;
+    if (voice != null) {
+      print(voice);
+    }
+  }
+
+  Future _speak(String txt) async {
+    await flutterTts.setVolume(volume);
+    await flutterTts.setSpeechRate(rate);
+    await flutterTts.setPitch(pitch);
+
+    if (txt != "") {
+        await flutterTts.speak(txt);
+    }
+  }
+
+  Future _setAwaitOptions() async {
+    await flutterTts.awaitSpeakCompletion(true);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    flutterTts.stop();
+  }
+
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(
@@ -57,9 +177,8 @@ class _MyHomePageState extends State<MyHomePage> {
               ElevatedButton(onPressed: (){
                 Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => RecordPage(camera: widget.camera)),
-            );
-              }, child : const Text('촬영모드')),
+                MaterialPageRoute(builder: (context) => RecordPage(camera: widget.camera, tts: flutterTts)),
+              );}, child : const Text('촬영모드')),
               ElevatedButton(onPressed: (){
                 Navigator.push(
                 context,
@@ -74,7 +193,6 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
 class AlbumRoute extends StatelessWidget {
   const AlbumRoute({super.key});
   @override
@@ -96,16 +214,17 @@ class AlbumRoute extends StatelessWidget {
 }
 class RecordPage extends StatefulWidget{
   final CameraDescription camera;
-  RecordPage({super.key, required this.camera});
+  final FlutterTts tts;
+  RecordPage({super.key, required this.camera, required this.tts});
   @override
   State<RecordPage> createState() => _RecordPageState();
 }
 class _RecordPageState extends State<RecordPage> {
   late CameraController _controller;
   late Future<void> _initializeControllerFuture;
-
   @override
   void initState() {
+    _speak("촬영모드입니다.");
     super.initState();
     // To display the current output from the Camera,
     // create a CameraController.
@@ -118,7 +237,11 @@ class _RecordPageState extends State<RecordPage> {
     // Next, initialize the controller. This returns a Future.
     _initializeControllerFuture = _controller.initialize();
   }
-
+  Future _speak(String txt) async {
+    if (txt != "") {
+        await widget.tts.speak(txt);
+    }
+  }
   @override
   void dispose() {
     // Dispose of the controller when the widget is disposed.
@@ -167,6 +290,7 @@ class _RecordPageState extends State<RecordPage> {
                   // Pass the automatically generated path to
                   // the DisplayPictureScreen widget.
                   imagePath: image.path,
+                  tts: widget.tts
                 ),
               ),
             );
@@ -184,7 +308,8 @@ class _RecordPageState extends State<RecordPage> {
 // A widget that displays the picture taken by the user.
 class DisplayPictureScreen extends StatefulWidget {
   final String imagePath;
-  const DisplayPictureScreen({super.key, required this.imagePath});
+  final FlutterTts tts;
+  const DisplayPictureScreen({super.key, required this.imagePath, required this.tts});
   @override
   DisplayPictureState createState() => DisplayPictureState();
 }
@@ -205,6 +330,12 @@ class DisplayPictureState extends State<DisplayPictureScreen>{
     // api 호출
     callApi(File(widget.imagePath)).then((value){
       print(value);
+      final ans = json.decode(value);
+      // widget.tts.speak(ans["ans"]);
+      getTranslation_papago(ans["ans"]).then((value){
+        widget.tts.speak(value);
+      });
+      // widget.tts.speak(res);
     });
 
     super.initState();
@@ -220,3 +351,32 @@ class DisplayPictureState extends State<DisplayPictureScreen>{
   }
 }
 
+
+Future getTranslation_papago(String txt) async {
+    String _client_id = "WYX9Cy9eD1ffV5IUEDAo";
+    String _client_secret = "n2AsCIc6L6";
+    String _content_type = "application/x-www-form-urlencoded; charset=UTF-8";
+    String _url = "https://openapi.naver.com/v1/papago/n2mt";
+    
+
+    http.Response trans = await http.post(
+      Uri.parse(_url),
+      headers: {
+        'Content-Type': _content_type,
+        'X-Naver-Client-Id': _client_id,
+        'X-Naver-Client-Secret': _client_secret
+      },
+      body: {
+        'source': "en",//위에서 언어 판별 함수에서 사용한 language 변수
+        'target': "ko",//원하는 언어를 선택할 수 있다. 
+        'text': txt,
+      },
+    );
+    if (trans.statusCode == 200) {
+      var dataJson = jsonDecode(trans.body);
+      var resultPapago = dataJson['message']['result']['translatedText'];
+      return resultPapago;
+    } else {
+      return trans.statusCode;
+    }
+  }
